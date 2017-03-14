5ca3c5bafaaf515a2ae4eb17346e76f35d01c73f
JunaidAhmed <junaidahmed.jk@gmail.com>
2017-03-14 08:27:39 +0500

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,21 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+// Object Destructuring Syntax ES6 Feature
+var {Route, Router, IndexRoute, hashHistory} = require("react-router");
+var Main = require("Main");
+
+
+// load foundation
+require('style-loader!css-loader!foundation-sites/dist/css/foundation.min.css');
+$(document).foundation();
+
+// App css
+require('style-loader!css-loader!sass-loader!applicationStyles');
+
+ReactDOM.render(
+	<Router history={hashHistory}>
+		<Route path="/" component={Main}>
+		</Route>
+	</Router>,
+	document.getElementById("app")
+);
