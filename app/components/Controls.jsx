bf2d50576035c75a9383c0b076b4e5bff3ac420c
JunaidAhmed <junaidahmed.jk@gmail.com>
2017-03-18 00:51:58 +0500

    Created Timer component

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -19,7 +19,7 @@ var Controls = React.createClass({
 			if (countdownStatus === "started") {
 				return <button className="button secondary" onClick={this.onStatusChange("paused")}>Pause</button>
 			}
-			else if (countdownStatus === "paused") {
+			else /*if (countdownStatus === "paused")*/ {
 				return <button className="button primary" onClick={this.onStatusChange("started")}>Start</button>
 			}
 		};
